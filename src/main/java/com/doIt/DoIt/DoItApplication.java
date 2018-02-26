d845babe0ed7b4f238e72ecf94db4f4d0c319b86
Sydbob <kiva1102@Gmail.com>
2018-02-26 14:31:21 +0000

    Initial commit

---
+++ src/main/java/com/doIt/DoIt/DoItApplication.java
@@ -0,0 +1,12 @@
+package com.doIt.DoIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoItApplication.class, args);
+	}
+}
