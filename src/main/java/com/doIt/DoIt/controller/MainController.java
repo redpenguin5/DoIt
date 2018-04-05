831fceb0a733605f239b346c84d2d1b355da6c83
Tom Smith <Tom@newhuaweiapstudent-10-53-10-226.ncl.ac.uk>
2018-04-05 10:38:47 +0100

    TS validatoin checks

--- src/main/java/com/doIt/DoIt/controller/MainController.java
+++ src/main/java/com/doIt/DoIt/controller/MainController.java
@@ -4,11 +4,12 @@ import com.doIt.DoIt.entity.Task;
 import com.doIt.DoIt.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
+import javax.validation.Valid;
+import org.springframework.validation.BindingResult;
 
 //notes: use rest controller, return response obj not Strings
 @Controller
@@ -58,6 +59,18 @@ public class MainController {
         request.setAttribute("mode", "MODE_UPDATE");
         return "update";
     }
+
+    //Addition validation check method, T Smith 3/4/18 unsure on  "/check-task" and "redirect:/results"
+    @PostMapping("/check-task")
+    public String checkTaskInfo(@Valid Task task, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "form";
+        }
+        return "redirect:/index";
+    }
+
+
+
 
 
 }
--- src/main/java/com/doIt/DoIt/entity/Task.java
+++ src/main/java/com/doIt/DoIt/entity/Task.java
@@ -2,47 +2,68 @@ package com.doIt.DoIt.entity;
 
 import javax.persistence.*;
 import java.io.Serializable;
+//Additon
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity(name="Task")
 public class Task implements Serializable {
 
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
+
+    //Additions standard validation annotations Tom S, 3/4/18
+    @NotNull
     private int taskID;
+
+    @NotNull
+    @Size(max=50)
     private String name;
+
+    @NotNull
+    @Size(max=300)
     private String description;
+
+    @NotNull
+    @Size(max=30)
     private String status;
+
+    @NotNull
     private int hours_contributed;
+
+    @NotNull
     private int hours_estimated;
+
+    @NotNull
     private int projectID;
 
     public Task() {
     }
 
 //TH and TS edited on 16.03 for validity checks on Task contrsuctor. There are two constructors so we weren't sure if the validty checks were needed on both?
-    
+
     public Task(String name, String description, String status, int hours_contributed, int hours_estimated, int projectID) {
         super();
-        
+
         if(name.length() > 50 || name == null) {
-        	throw new IllegalArgumentException("Task name must be specified as a string under 50 characters.");
+            throw new IllegalArgumentException("Task name must be specified as a string under 50 characters.");
         }
         else this.name = name;
-        
+
         if(description.length() > 300 || description == null) {
-        	throw new IllegalArgumentException("Task description must be specified as a string under 300 characters.");
+            throw new IllegalArgumentException("Task description must be specified as a string under 300 characters.");
         }
         else this.description = description;
-        
+
         if(status.length() > 30 || status == null) {
-        	throw new IllegalArgumentException("Task status must be specified as a string under 30 characters.");
+            throw new IllegalArgumentException("Task status must be specified as a string under 30 characters.");
         }
-        else this.status = status; 
-        
+        else this.status = status;
+
         this.hours_contributed = hours_contributed;
-        
+
         this.hours_estimated = hours_estimated;
-            
+
 
         this.projectID = projectID;
     }
@@ -91,7 +112,7 @@ public class Task implements Serializable {
 
     public void setName(String name) {
         if(name.length() > 50 || name == null) {
-        	throw new IllegalArgumentException("Task name must be specified as a string under 50 characters.");
+            throw new IllegalArgumentException("Task name must be specified as a string under 50 characters.");
         }
         else this.name = name;
     }
@@ -104,7 +125,7 @@ public class Task implements Serializable {
 
     public void setStatus(String status) {
         if(status.length() > 30 || status == null) {
-        	throw new IllegalArgumentException("Task status must be specified as a string under 30 characters.");
+            throw new IllegalArgumentException("Task status must be specified as a string under 30 characters.");
         }
         else this.status = status;
     }
@@ -114,7 +135,7 @@ public class Task implements Serializable {
         this.hours_contributed = hours_contributed;}
 
     public void setHours_estimated(int hours_estimated) {
-       this.hours_estimated = hours_estimated;}
+        this.hours_estimated = hours_estimated;}
 
     public void setProjectID(int projectID) {
         this.projectID = projectID;}
@@ -131,6 +152,6 @@ public class Task implements Serializable {
                 ", projectID=" + projectID +
                 '}';
     }
-      
+
 
 }
